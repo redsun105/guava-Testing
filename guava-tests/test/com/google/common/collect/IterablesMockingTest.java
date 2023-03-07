cacb0318a5b6117572b4bdd0605c5533c807e494
Xiaowen Sun <xiaowensun0212@gmail.com>
2023-03-07 15:02:24 -0800

    update a mock test for Iterables.all

--- guava-tests/test/com/google/common/collect/IterablesMockingTest.java
+++ guava-tests/test/com/google/common/collect/IterablesMockingTest.java
@@ -6,8 +6,7 @@ import org.junit.Test;
 import java.util.Arrays;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class IterablesMockingTest {
     @Test
@@ -23,6 +22,13 @@ public class IterablesMockingTest {
 
         // Call the Iterables.all method with the mocked Iterable and Predicate
         boolean allEven = Iterables.all(iterable, evenPredicate);
+
+        // Verify that the iterable's iterator method was called on the mock objects with the correct arguments
+        verify(iterable).iterator();
+
+        // Verify that the evenPredicate's apply method was called on the mock objects with the correct arguments
+        verify(evenPredicate).apply(2);
+        verify(evenPredicate).apply(3);
 
         // Verify that the method returns the expected result
         assertEquals(false, allEven);
